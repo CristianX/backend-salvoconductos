81bd697c98bd7ea383fc7c25bd3f2168c65a8c53
CristianX <thecristianx@hotmail.com>
2021-10-18 14:04:45 -0500

    Añadiendo id de request para identificar la petición con morgan

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,10 +1,28 @@
 const { createLogger, format, transports } = require('winston');
+const morgan = require('morgan');
+const stripFinalNewline = require('strip-final-newline');
 
 // Setup Logger
-
 const logger = createLogger({
     format: format.simple(),
     transports: [new transports.Console()],
 });
+
+// Setup requets logger
+morgan.token('id', (req) => req.id);
+
+const requestFormat = ':remote-addr [:date[iso]] :id ":method :url" :status';
+const requests = morgan(requestFormat, {
+    stream: {
+        write: (message) => {
+            // Remove all line breaks
+            const log = stripFinalNewline(message);
+            return logger.info(log);
+        },
+    },
+});
+
+// Attach to logger onject
+logger.requests = requests;
 
 module.exports = logger;
